6ebc2951759b522a95b9a2c47890fde38ab7e827
Lucas Dasso <lucas.dasso@wundermanthompson.com>
2021-07-30 10:54:07 -0300

    Improved bitbucket test response

--- src/public/index.php
+++ src/public/index.php
@@ -2,32 +2,36 @@
 include '../functions.php';
 $toDeployFolder = __DIR__ . '/../' . $toDeployFolderName;
 
-
 /*
 * SCRIPT
 ************************/
 
 //Get webhook body json
 $body_raw = trim(file_get_contents('php://input'));
-// $body_raw = trim(file_get_contents('../post_body.json'));
 
 if(!$body_raw){
     die('Nothing to do.');
 }
 
-// Check if HMAC match
-if (!isValidHMAC($_SERVER, $body_raw, $secret)) {
-    echo "error";
-    die('Invalid HMAC hash.');
+$body = json_decode($body_raw); // Get webhook data
+
+if(isset($_SERVER['HTTP_X_EVENT_KEY']) && $_SERVER['HTTP_X_EVENT_KEY'] == 'diagnostics:ping'){
+    if(@$body->test){
+        die('Success!');
+    }
+    die('Fail to receive test body value.');
 }
 
-$body = json_decode($body_raw); // Get webhook data
+// Check if HMAC match
+if (!isValidHMAC($_SERVER, $body_raw, $secret)) {
+    die('Invalid HMAC hash.');
+}
 
 //Generate the repo rull name
 $repoFullName = $body->repository->project->key . '_' . $body->repository->name;
 
 //Get repo updated branch name
-$repoBranchName = trim(strtolower($body->repository->changes->ref->displayId));
+$repoBranchName = trim(strtolower($body->changes[0]->ref->displayId));
 if( !in_array($repoBranchName,['main', 'master'])){
     die('Only "main" or "master" branchs can be deployed.');
 }
